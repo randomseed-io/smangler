615ea3d7b94061ddce369765308b869f4916c14c
Paweł Wilk <siefca@gnu.org>
2019-08-13 12:44:05 +0200

    Better handling of mixes sequences (chars, numbers) during conversion

--- src/smangler/proto.clj
+++ src/smangler/proto.clj
@@ -4,12 +4,7 @@
       :author "Paweł Wilk"
       :added  "1.0.0"}
 
-    smangler.proto
-
-  (:require [clojure.string  :as            str]
-            [smangler.core   :as             sc]
-            [smangler.spec   :as              s]
-            [orchestra.core  :refer [defn-spec]]))
+    smangler.proto)
 
 (defprotocol Stringable
 
@@ -22,6 +17,10 @@
   (->str [w] w)
 
   clojure.lang.Seqable
+
+  (->str [w] (apply str w))
+
+  CharSequence
 
   (->str [w] (apply str w))
 
@@ -39,37 +38,42 @@
 
 (defprotocol Predicative
 
-  (->char-pred [p])
-  (->part-pred [p]))
+  (->char-match [p])
+  (->part-pred  [p]))
 
 (extend-protocol Predicative
 
-  clojure.lang.Seqable
+  clojure.lang.Sequential
 
-  (->char-pred [p] (set p))
-  (->part-pred [p] (set p))
+  (->char-match [p] (set (apply str p)))
+  (->part-pred  [p] (set (apply str p)))
+
+  CharSequence
+
+  (->char-match [p] (set p))
+  (->part-pred  [p] (set p))
 
   clojure.lang.IPersistentVector
 
-  (->char-pred [p] (set p))
-  (->part-pred [p] (set p))
+  (->char-match [p] (set (apply str p)))
+  (->part-pred  [p] (set (apply str p)))
 
   clojure.lang.IFn
 
-  (->char-pred [p] p)
-  (->part-pred [p] p)
+  (->char-match [p] p)
+  (->part-pred  [p] p)
 
   Character
 
-  (->char-pred [p] #(and (= p %) %))
-  (->part-pred [p] #(= p %))
+  (->char-match [p] #(and (= p %) %))
+  (->part-pred  [p] #(= p %))
 
   Number
 
-  (->char-pred [p] (->char-pred (str p)))
-  (->part-pred [p] (->part-pred (str p)))
+  (->char-match [p] (set (str p)))
+  (->part-pred  [p] (set (str p)))
 
   nil
 
-  (->char-pred [p] (constantly nil))
-  (->part-pred [p] (constantly nil)))
+  (->char-match [p] (constantly nil))
+  (->part-pred  [p] (constantly nil)))
