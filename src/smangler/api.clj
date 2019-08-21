9bf9a57e08111b8ed4e0218fab6b6c6139cf6d4f
Paweł Wilk <siefca@gnu.org>
2019-08-21 22:27:49 +0200

    Documentation strings in smangler.api moved to defdoc! bodies

--- src/smangler/api.clj
+++ src/smangler/api.clj
@@ -15,6 +15,117 @@
 (defn-spec trim-same ::s/phrase
   "Takes a string and recursively trims its first and last character if they are equal.
   Returns a new string or nil when nil was passed as an argument instead of a
+  string."
+  {:added "1.0.0" :tag String}
+
+  ([^String w ::s/stringable]
+   (sc/trim-same (->str w)))
+
+  ([^clojure.lang.IFn matcher ::s/char-matchable
+    ^String w                 ::s/stringable]
+   (sc/trim-same (->character-matcher matcher) (->str w)))
+
+  ([^Character start ::s/beginning-character
+    ^Character   end ::s/ending-character
+    ^String        w ::s/stringable]
+   (sc/trim-same start end (->str w))))
+
+(defn-spec trim-same-once ::s/phrase
+  "Takes a string and trims its first and last character if they are equal.
+  Returns a new string or original string if there is nothing to trim."
+  {:added "1.0.0" :tag String}
+
+  ([^String w ::s/stringable]
+   (some-or sc/trim-same-once (->str w)))
+
+  ([^clojure.lang.IFn matcher ::s/char-matchable
+    ^String w                 ::s/stringable]
+   (some-or sc/trim-same-once (->character-matcher matcher) (->str w)))
+
+  ([^Character start ::s/beginning-character
+    ^Character   end ::s/ending-character
+    ^String        w ::s/stringable]
+   (some-or sc/trim-same-once start end (->str w))))
+
+(defn-spec trim-same-seq ::s/lazy-seq-of-strings
+  "Takes a string and recursively trims its first and last character if they are
+  equal. Returns a lazy sequence of strings for each iteration."
+  {:added "1.0.0" :tag clojure.lang.LazySeq}
+
+  ([^String w ::s/stringable]
+   (part-caller-iterate sc/trim-same-once (->str w)))
+
+  ([^clojure.lang.IFn matcher ::s/char-matchable
+    ^String                 w ::s/stringable]
+   (part-caller-iterate sc/trim-same-once (->character-matcher matcher) (->str w)))
+
+  ([^Character start ::s/beginning-character
+    ^Character   end ::s/ending-character
+    ^String        w ::s/stringable]
+   (part-caller-iterate sc/trim-same-once start end (->str w))))
+
+(defn-spec trim-same-once-with-orig ::s/one-or-two-strings
+  "Takes a string and trims its first and last character if they are equal.
+  Returns a 2-element sequence containing a new string and the original one."
+  {:added "1.0.0" :tag clojure.lang.LazySeq}
+
+  ([^CharSequence w ::s/stringable]
+   (when-some [w (->str w)]
+     (cons w (when-some [r (sc/trim-same-once w)]
+               (cons r nil)))))
+
+  ([^clojure.lang.IFn matcher ::s/char-matchable
+    ^CharSequence           w ::s/stringable]
+   (when-some [w (->str w)]
+     (cons w (when-some [r (sc/trim-same-once (->character-matcher matcher) w)]
+               (cons r nil)))))
+
+  ([^Character start ::s/beginning-character
+    ^Character   end ::s/ending-character
+    ^CharSequence  w ::s/stringable]
+   (when-some [w (->str w)]
+     (cons w (when-some [r (sc/trim-same-once start end w)]
+               (cons r nil))))))
+
+(defn-spec all-prefixes ::s/lazy-seq-of-ne-strings
+  "Generates a lazy sequence of all possible prefixes of a given string."
+  {:added "1.0.0" :tag clojure.lang.LazySeq}
+
+  ([^String w ::s/stringable]
+   (sc/all-prefixes (->str w)))
+
+  ([^clojure.lang.IFn pred ::s/phrase-splittable
+    ^String              w ::s/stringable]
+   (sc/all-prefixes (->string-partitioner pred) (->str w))))
+
+(defn-spec all-suffixes ::s/lazy-seq-of-ne-strings
+  "Generates a lazy sequence of all possible suffixes of a given string."
+  {:added "1.0.0" :tag clojure.lang.LazySeq}
+
+  ([^String w ::s/stringable]
+   (sc/all-suffixes (->str w)))
+
+  ([^clojure.lang.IFn pred ::s/phrase-splittable
+    ^String w              ::s/stringable]
+   (sc/all-suffixes (->string-partitioner pred) (->str w))))
+
+(defn-spec all-subs ::s/lazy-seq-of-ne-strings
+  "Generates a lazy sequence of all possible substrings (prefixes, suffixes and
+  infixes) of a given string."
+  {:added "1.0.0" :tag clojure.lang.LazySeq}
+
+  ([^String w ::s/stringable]
+   (sc/all-subs (->str w)))
+
+  ([^clojure.lang.IFn pred ::s/phrase-splittable
+    ^String              w ::s/stringable]
+   (sc/all-subs (->string-partitioner pred) (->str w))))
+
+;; Extended documentation
+
+(defdoc! trim-same
+  "Takes a string and recursively trims its first and last character if they are equal.
+  Returns a new string or nil when nil was passed as an argument instead of a
   string. For an empty string it returns an empty string.
 
   It automatically converts objects of the following types to strings: characters,
@@ -43,22 +154,9 @@
   When 3 arguments are given the first two should be characters used to match first
   and last character of a trimmed string (given as third argument).
 
-  When the string consist of 2 matching letters the result will be an empty string."
-  {:added "1.0.0" :tag String}
+  When the string consist of 2 matching letters the result will be an empty string.")
 
-  ([^String w ::s/stringable]
-   (sc/trim-same (->str w)))
-
-  ([^clojure.lang.IFn matcher ::s/char-matchable
-    ^String w                 ::s/stringable]
-   (sc/trim-same (->character-matcher matcher) (->str w)))
-
-  ([^Character start ::s/beginning-character
-    ^Character   end ::s/ending-character
-    ^String        w ::s/stringable]
-   (sc/trim-same start end (->str w))))
-
-(defn-spec trim-same-once ::s/phrase
+(defdoc! trim-same-once
   "Takes a string and trims its first and last character if they are equal.
   Returns a new string or original string if there is nothing to trim. For nil it
   returns nil.
@@ -89,22 +187,9 @@
   When 3 arguments are given the first two should be characters used to match first
   and last character of a trimmed string (given as third argument).
 
-  When the string consist of 2 matching letters the result will be an empty string."
-  {:added "1.0.0" :tag String}
+  When the string consist of 2 matching letters the result will be an empty string.")
 
-  ([^String w ::s/stringable]
-   (some-or sc/trim-same-once (->str w)))
-
-  ([^clojure.lang.IFn matcher ::s/char-matchable
-    ^String w                 ::s/stringable]
-   (some-or sc/trim-same-once (->character-matcher matcher) (->str w)))
-
-  ([^Character start ::s/beginning-character
-    ^Character   end ::s/ending-character
-    ^String        w ::s/stringable]
-   (some-or sc/trim-same-once start end (->str w))))
-
-(defn-spec trim-same-seq ::s/lazy-seq-of-strings
+(defdoc! trim-same-seq
   "Takes a string and recursively trims its first and last character if they are
   equal. Returns a lazy sequence of strings for each iteration. For nil it returns
   nil.
@@ -136,22 +221,9 @@
   and last character of a trimmed string (given as third argument).
 
   When the last of the processed strings consist of 2 matching letters the resulting
-  sequence will contain an empty string."
-  {:added "1.0.0" :tag clojure.lang.LazySeq}
+  sequence will contain an empty string.")
 
-  ([^String w ::s/stringable]
-   (part-caller-iterate sc/trim-same-once (->str w)))
-
-  ([^clojure.lang.IFn matcher ::s/char-matchable
-    ^String                 w ::s/stringable]
-   (part-caller-iterate sc/trim-same-once (->character-matcher matcher) (->str w)))
-
-  ([^Character start ::s/beginning-character
-    ^Character   end ::s/ending-character
-    ^String        w ::s/stringable]
-   (part-caller-iterate sc/trim-same-once start end (->str w))))
-
-(defn-spec trim-same-once-with-orig ::s/one-or-two-strings
+(defdoc! trim-same-once-with-orig
   "Takes a string and trims its first and last character if they are equal.
   Returns a 2-element sequence containing a new string and the original one. If there
   is nothing to trim it returns a sequence with just 1 element. For nil it returns
@@ -184,28 +256,9 @@
   and last character of a trimmed string (given as third argument).
 
   When the last of the processed strings consist of 2 matching letters the resulting
-  sequence will contain an empty string."
-  {:added "1.0.0" :tag clojure.lang.LazySeq}
+  sequence will contain an empty string.")
 
-  ([^CharSequence w ::s/stringable]
-   (when-some [w (->str w)]
-     (cons w (when-some [r (sc/trim-same-once w)]
-               (cons r nil)))))
-
-  ([^clojure.lang.IFn matcher ::s/char-matchable
-    ^CharSequence           w ::s/stringable]
-   (when-some [w (->str w)]
-     (cons w (when-some [r (sc/trim-same-once (->character-matcher matcher) w)]
-               (cons r nil)))))
-
-  ([^Character start ::s/beginning-character
-    ^Character   end ::s/ending-character
-    ^CharSequence  w ::s/stringable]
-   (when-some [w (->str w)]
-     (cons w (when-some [r (sc/trim-same-once start end w)]
-               (cons r nil))))))
-
-(defn-spec all-prefixes ::s/lazy-seq-of-ne-strings
+(defdoc! all-prefixes
   "Generates a lazy sequence of all possible prefixes of a given string. Returns nil
   if nil or an empty string was given as an argument instead of a string.
 
@@ -227,17 +280,9 @@
   collections of characters, collections of numbers. It splits them into
   single-character elements and creates a sets to be used as functions. In case of
   single characters it creates a small predicate function. For nil it will create a
-  function that won't match anything. Same for empty collections or strings."
-  {:added "1.0.0" :tag clojure.lang.LazySeq}
+  function that won't match anything. Same for empty collections or strings.")
 
-  ([^String w ::s/stringable]
-   (sc/all-prefixes (->str w)))
-
-  ([^clojure.lang.IFn pred ::s/phrase-splittable
-    ^String              w ::s/stringable]
-   (sc/all-prefixes (->string-partitioner pred) (->str w))))
-
-(defn-spec all-suffixes ::s/lazy-seq-of-ne-strings
+(defdoc! all-suffixes
   "Generates a lazy sequence of all possible suffixes of a given string. Returns nil
   if nil or an empty string was given as an argument instead of a string.
 
@@ -259,17 +304,9 @@
   collections of characters, collections of numbers. It splits them into
   single-character elements and creates a sets to be used as functions. In case of
   single characters it creates a small predicate function. For nil it will create a
-  function that won't match anything. Same for empty collections or strings."
-  {:added "1.0.0" :tag clojure.lang.LazySeq}
+  function that won't match anything. Same for empty collections or strings.")
 
-  ([^String w ::s/stringable]
-   (sc/all-suffixes (->str w)))
-
-  ([^clojure.lang.IFn pred ::s/phrase-splittable
-    ^String w              ::s/stringable]
-   (sc/all-suffixes (->string-partitioner pred) (->str w))))
-
-(defn-spec all-subs ::s/lazy-seq-of-ne-strings
+(defdoc! all-subs
   "Generates a lazy sequence of all possible substrings (prefixes, suffixes and
   infixes) of a given string. Returns nil if nil or an empty string was given as an
   argument instead of a string.
@@ -294,12 +331,4 @@
   collections of characters, collections of numbers. It splits them into
   single-character elements and creates a sets to be used as functions. In case of
   single characters it creates a small predicate function. For nil it will create a
-  function that won't match anything. Same for empty collections or strings."
-  {:added "1.0.0" :tag clojure.lang.LazySeq}
-
-  ([^String w ::s/stringable]
-   (sc/all-subs (->str w)))
-
-  ([^clojure.lang.IFn pred ::s/phrase-splittable
-    ^String              w ::s/stringable]
-   (sc/all-subs (->string-partitioner pred) (->str w))))
+  function that won't match anything. Same for empty collections or strings.")
